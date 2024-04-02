1271579d7a1fb6f2e02785fda6d976c3bbe2daa3
SaranNirmala <msaranyanirmala@gmail.com>
2024-04-02 12:59:26 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
-import { Navbar } from "./Components/Navbar/navbar";
-import Home from "./Components/Home/Home";
-import About from "./Components/About/About";
 import Pages from "./Components/Home/Pages";
 import Certificate from "./Components/Certificate/Certificate";
 
