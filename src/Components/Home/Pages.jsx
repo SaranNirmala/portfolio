7ff756995e254e85fb2281bcf5c52c751abad3ce
SaranNirmala <126362188+SaranNirmala@users.noreply.github.com>
2024-04-02 13:05:45 +0530

    Update Pages.jsx

--- src/Components/Home/Pages.jsx
+++ src/Components/Home/Pages.jsx
@@ -2,7 +2,7 @@ import About from "../About/About";
 import Contact from "../Contact/Contact";
 import Experience from "../Experience/Experience";
 import Footer from "../Footer/Footer";
-import { Navbar } from "../Navbar/navbar";
+import { Navbar } from "../Navbar/Navbar";
 import Projects from "../Projects/Projects";
 import Skills from "../Skills/Skills";
 import Home from "./Home"
